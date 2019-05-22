aad9c22bca42dff7aee058eaf4245c78520d4845
malik masis <m.masis@amatis.nl>
2019-05-22 12:35:50 +0300

    Add project files.

---
+++ SehirRehberi.API/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SehirRehberi.API.Models;
+
+namespace SehirRehberi.API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
